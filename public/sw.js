7d052e1fb29a4310858e28444874d2cef359f78e
ntthanhvy <nguyenthithanhvyjps15@gmail.com>
2020-07-16 17:25:49 +0700

    remove pwa

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-3b38d5bd'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "dadb8e476287412bb53cc6eccdbccf88"
+    "revision": "6947aec439dc7ef774edcf1ec0efaa76"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,25 +32,6 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
             rel="stylesheet"
           ></link>
           <meta charSet="utf-8" />
-          {/* <meta charset="utf-8" /> */}
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-          />
-          <meta name="description" content="Description" />
-          <meta name="keywords" content="Keywords" />
-
-          <link rel="manifest" href="/manifest.json" />
-          {/* <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" /> */}
-          {/* <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" /> */}
-          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-          <meta name="theme-color" content="#317EFB" />
-
-          <link
-            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-            rel="stylesheet"
-          />
         </Head>
         <Header cartCount={cart.length} />
         <Component {...pageProps} cart={cart} setCart={setCart} />
